30cab9e23febb413e0fe5cf138558596a1e068ba
jarrettdylan <141463046+jarrettdylan@users.noreply.github.com>
2025-09-30 23:37:19 -0400

    created stats function file and updated main to return character count

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(f"Found {num_words} total words")
+    print(chars_dict)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-
+from stats import get_num_words
+from stats import get_chars_dict
 main()
