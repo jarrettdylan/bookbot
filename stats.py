934571b25d236ff181cb916dc3d285e92fb051b3
jarrettdylan <141463046+jarrettdylan@users.noreply.github.com>
2025-09-30 23:51:30 -0400

    Finished project

--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
+
 def get_chars_dict(text):
     chars = {}
     for c in text:
@@ -10,4 +11,16 @@ def get_chars_dict(text):
             chars[lowered] += 1
         else:
             chars[lowered] = 1
-    return chars    
+    return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
